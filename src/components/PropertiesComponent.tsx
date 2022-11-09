e0069431375ecc5bb25dcf4fc8a38a281ea36381
Ilias Soto <iliassoto@hotmail.com>
2022-11-09 11:28:08 +0100

    Fix error for nested array objects in config file

--- src/components/PropertiesComponent.tsx
+++ src/components/PropertiesComponent.tsx
@@ -3,12 +3,12 @@ import 'github-markdown-css/github-markdown.css';
 import { Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 import CodeViewComponent from './CodeViewComponent';
 
-export function createPropertiesComponent(props: {obj?: any, properties?: {key: string, value: any}[], propsToIgnore?: string[], colHeader?: string, width?: string, rowHeight?: string}): JSX.Element | undefined {
+export function createPropertiesComponent(props: {obj?: any, properties?: {key: string, value: any}[], propsToIgnore?: string[], colHeader?: string, width?: string, rowHeight?: string, marginTop?: string}): JSX.Element | undefined {
   var rows = props.properties ?? [];
   if (props.obj && typeof props.obj == 'object') rows = rows.concat(Object.keys(props.obj).map(key => {return {key: key, value: props.obj[key]}}));
   const propsToIgnore = props.propsToIgnore || [];
   const entries = rows.filter(row => !propsToIgnore.includes(row.key));
-  if (entries.length > 0) return <PropertiesComponent entries={entries} width={props.width} rowHeight={props.rowHeight} colHeader={props.colHeader} />;
+  if (entries.length > 0) return <PropertiesComponent entries={entries} width={props.width} rowHeight={props.rowHeight} colHeader={props.colHeader} marginTop={props.marginTop} />;
 }
 
 function removeBlockOfTrailingSpaces(code: string): string {
@@ -22,7 +22,7 @@ function removeBlockOfTrailingSpaces(code: string): string {
  * Recursion is supported.
  * @param colHeader a optional string which is displayed as an additional column on the left
  */
-export default function PropertiesComponent(props: {entries: {key: string, value: any}[], colHeader?: string, width?: string, rowHeight?: string}){
+export default function PropertiesComponent(props: {entries: {key: string, value: any}[], colHeader?: string, width?: string, rowHeight?: string, marginTop?: string}){
   let width = props.width || "fit-content";
   const rows = props.entries
   .map((entry,idx) => {
@@ -31,9 +31,12 @@ export default function PropertiesComponent(props: {entries: {key: string, value
     // pass through react elements
     if (React.isValidElement(value)) {} // no operation
     // pass through list of react elements
-    else if (Array.isArray(value) || React.isValidElement(value[0])) {} // no operation
+    else if (Array.isArray(value) && React.isValidElement(value[0])) {} // no operation
+    // map every array element to react element
+    else if (Array.isArray(value)) {
+      value = value.map((e,idx) => createPropertiesComponent({obj: e, marginTop: (idx==0 ? "0px" : "8px")}));
     // create a key/value table for objects
-    else if (typeof value == 'object' && !Array.isArray(value)) {
+    } else if (typeof value == 'object' && !Array.isArray(value)) {
       value = createPropertiesComponent({obj: value});
     } else if (key === "code") {
     // format code
@@ -56,7 +59,7 @@ export default function PropertiesComponent(props: {entries: {key: string, value
     return undefined;
   })
   .filter(cmp => cmp); // remove undefined values;
-  return <TableContainer component={Paper} sx={{width: width}}>
+  return <TableContainer component={Paper} sx={{width: width, mt: props.marginTop}}>
     <Table size="small">
       <TableBody>
         {rows}
