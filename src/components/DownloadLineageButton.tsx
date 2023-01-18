9a9c6a6ed6c6952f1444c79cae70ae1e3aef5fe4
Ilias Soto <iliassoto@hotmail.com>
2023-01-18 10:08:55 +0100

    Changed one comment

--- src/components/DownloadLineageButton.tsx
+++ src/components/DownloadLineageButton.tsx
@@ -4,8 +4,7 @@ import { toPng } from 'html-to-image';
 
 function downloadImage(dataUrl: string) {
   const a = document.createElement('a');
-
-  a.setAttribute('download', 'reactflow.png');
+  a.setAttribute('download', 'lineage.png');
   a.setAttribute('href', dataUrl);
   a.click();
 }
@@ -15,7 +14,7 @@ function DownloadButton() {
     toPng(document.querySelector('.react-flow') as HTMLElement, {
       filter: (node) => {
         console.log(node.classList)
-        // we don't want to add the minimap and the controls to the image
+        // don't include minimap, the controls and the MUI Buttons.
         if (
           node?.classList?.contains('react-flow__minimap') ||
           node?.classList?.contains('react-flow__controls') ||
@@ -31,7 +30,6 @@ function DownloadButton() {
   };
 
   return (
-
       <Button
           variant='contained'
           startIcon={<CloudDownloadIcon />}
