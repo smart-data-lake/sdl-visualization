de40486a60238d088ce7fc87348e66019acac7cb
Entouanes <38790575+Entouanes@users.noreply.github.com>
2023-04-06 13:17:19 +0200

    Save small changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ConfigExplorer from './components/ConfigExplorer/ConfigExplorer';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <HashRouter>
     <React.StrictMode>
-      <App />
+      <ConfigExplorer />
     </React.StrictMode>
+  </HashRouter>
 );
