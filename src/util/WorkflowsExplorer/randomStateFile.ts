9969af1a5fc866399ab0adf160bf18c4ff2d7be8
Entouanes <38790575+Entouanes@users.noreply.github.com>
2023-04-13 09:03:48 +0200

    Started working on backend branch

--- src/util/WorkflowsExplorer/randomStateFile.ts
+++ src/util/WorkflowsExplorer/randomStateFile.ts
@@ -126,14 +126,3 @@ export function generateRandomDB() {
 
     //exportData(db)
 }
-
-/* const exportData = (data: any) => {
-    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-      JSON.stringify(data)
-    )}`;
-    const link = document.createElement("a");
-    link.href = jsonString;
-    link.download = "data.json";
-
-    link.click();
-}; */
