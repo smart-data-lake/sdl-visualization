243750f71e48aff033e29eb88939fd96d590b1e7
Entouanes <38790575+Entouanes@users.noreply.github.com>
2023-05-22 10:39:10 +0200

    Implemented temporary version to handle missing local statefiles

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,31 +1,47 @@
-import { List, ListItem/* , ListItemContent */, ListItemButton, Sheet, Box, Divider } from '@mui/joy';
+import { List, ListItem/* , ListItemContent */, ListItemButton, Sheet, Box, Divider, Tooltip } from '@mui/joy';
 import React from 'react'
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import SpeedRoundedIcon from '@mui/icons-material/SpeedRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import TuneRoundedIcon from '@mui/icons-material/TuneRounded';
 import { useNavigate } from 'react-router-dom';
+import { useManifest } from '../hooks/useManifest';
 
 /**
  * The SideBar is a navigation bar that is fixed on the left side of the screen. It contains buttons that allow the user to navigate to different pages, such as the Home page, Workflows Explorer page, and Config Viewer page.
  * @returns JSX element that represents the SideBar component
  */
 const SideBar = () => {
+    const {data, isLoading} = useManifest();
+    const navigate = useNavigate();
+    
+    if (isLoading) return <></>
+    
     const buttons = [
         {
+            name : 'home',
             icon : <HomeRoundedIcon/>,
-            link : '/'
+            link : '/',
+            disabled : false,
+            filetype : 'none'
         },
         {
+            name : 'workflows explorer',
             icon : <SpeedRoundedIcon/>,
-            link : '/workflows'
+            link : '/workflows',
+            disabled : !(data.localSetup && data.fallbackStatefiles),
+            filetype : 'state'
         },
         {
+            name : 'config viewer',
             icon : <TuneRoundedIcon/>,
-            link : '/config'
+            link : '/config',
+            disabled : !(data.localSetup && data.fallbackConfigfiles),
+            filetype : 'config'
         }
     ]
-    const navigate = useNavigate();
+
+    console.log(buttons)
 
     return ( 
         <Sheet
@@ -56,16 +72,35 @@ const SideBar = () => {
                     {buttons.map((component) => (
                         <>    
                             <ListItem>
-                                <ListItemButton 
-                                    sx={{
-                                        borderRadius: 4, 
-                                        scale: '90%',
-                                        justifyContent: 'center',
-                                    }}
-                                    onClick={() => navigate(component.link)}
-                                >
-                                    {component.icon}
-                                </ListItemButton>
+                                {!component.disabled && (
+                                    <ListItemButton 
+                                            sx={{
+                                                borderRadius: 4, 
+                                                scale: '90%',
+                                                justifyContent: 'center',
+                                            }}
+                                            onClick={() => navigate(component.link)}
+                                        >   
+                                            {component.icon}
+                                    </ListItemButton>
+                                )}
+                                {component.disabled && (
+                                    <Tooltip title={`No data was found for the menu "${component.name}". Please check that the ${component.filetype} files are at the expected location according to the manifest.`} placement='right'>
+                                        <div>
+                                        <ListItemButton 
+                                            sx={{
+                                                borderRadius: 4, 
+                                                scale: '90%',
+                                                justifyContent: 'center',
+                                            }}
+                                            onClick={() => navigate(component.link)}
+                                            disabled
+                                            >   
+                                            {component.icon}
+                                        </ListItemButton>
+                                        </div>
+                                    </Tooltip>
+                                )}
                             </ListItem>
                         </>
                     ))}
