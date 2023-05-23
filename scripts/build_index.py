9fce268d7278334ff920d05229566aef8a908704
Entouanes <38790575+Entouanes@users.noreply.github.com>
2023-05-23 14:44:33 +0200

    Small correction to the index builder to accomodate new manifest paradigm regarding missing files

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -164,12 +164,13 @@ def main():
     print(f"Retrieving summaries \"{path}\"...")
     files = list_files(path, ".json")
     print(f"{len(files)} files found.")
-    print("Creating summaries...")
-    db = create_dict(files)
-    print("Writing summaries...")
-    with open(f"{path}/index.json", "w") as outfile:
-        json.dump(db, outfile, ensure_ascii=False, indent=4)
-    print("Summaries written. \n \n")
+    if len(files) > 0:
+        print("Creating summaries...")
+        db = create_dict(files)
+        print("Writing summaries...")
+        with open(f"{path}/index.json", "w") as outfile:
+            json.dump(db, outfile, ensure_ascii=False, indent=4)
+        print("Summaries written. \n \n")
 
     print("The tool will compile all config files in \"/public/config\" and its subdirectories into \"/public/config/index.json\". If no config files are present, a default empty index is returned:")
     
@@ -177,12 +178,13 @@ def main():
     print(f"Retrieving configs \"{path}\"...")
     files = list_files(path, ".conf")
     print(f"{len(files)} files found.")
-    print("Creating index...")
-    db = [f.split("scripts/../public/config")[1] for f in files]
-    print("Writing summaries...")
-    with open(f"{path}/index.json", "w") as outfile:
-        json.dump(db, outfile, ensure_ascii=False, indent=4)
-    print("Summaries written. \n \n")
+    if len(files) > 0:
+        print("Creating index...")
+        db = [f.split("scripts/../public/config")[1] for f in files]
+        print("Writing summaries...")
+        with open(f"{path}/index.json", "w") as outfile:
+            json.dump(db, outfile, ensure_ascii=False, indent=4)
+        print("Summaries written. \n \n")
     
 
     
