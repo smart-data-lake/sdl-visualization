60efafc95b609259fc253467e281b213baf32a06
zkull <zach.kull@gmail.com>
2023-07-23 00:39:11 +0200

    move search/filter state from ElementList up to ConfigExplorer

--- src/components/ConfigExplorer/ConfigExplorer.tsx
+++ src/components/ConfigExplorer/ConfigExplorer.tsx
@@ -1,5 +1,5 @@
 import { Sheet } from '@mui/joy';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import PageHeader from '../../layouts/PageHeader';
 import './App.css';
@@ -14,12 +14,47 @@ function ConfigExplorer(props: { data: any }) {
 	const { data } = props;
 	const [listWidth, setListWidth] = useState(250); // initial width of left element
 	const listRef = useRef<HTMLDivElement>(null);
+	const [filter, setFilter] = useState<string>();
+	const [allDataObjects, setAllDataObjects] = useState<string[]>([]);
+	const [allActions, setAllActions] = useState<string[]>([]);
+	const [allConnections, setAllConnections] = useState<string[]>([]);	
+	const [currentDataObjects, setCurrentDataObjects] = useState(allDataObjects);
+	const [currentActions, setCurrentActions] = useState<string[]>(allActions);
+	const [currentConnections, setCurrentConnections] = useState<string[]>(allConnections);
+
+	useEffect(() => {
+		if (data) {
+			if (data.dataObjects) {
+				setAllDataObjects(Object.keys(data.dataObjects).sort());
+			}
+			if (data.actions) {
+				setAllActions(Object.keys(data.actions).sort());
+			}
+			if (data.connections) {
+				setAllConnections(Object.keys(data.connections).sort());
+			}
+		}
+	}, [data]);
+
+	useEffect(() => {
+		if (filter && filter.length>0) {
+			const searchText = filter.toLowerCase()
+			setCurrentDataObjects(allDataObjects.filter(a => a.toLowerCase().includes(searchText)));
+			setCurrentActions(allActions.filter(a => a.toLowerCase().includes(searchText)));
+			setCurrentConnections(allConnections.filter(a => a.toLowerCase().includes(searchText)));
+		} else {
+			setCurrentDataObjects(allDataObjects);
+			setCurrentActions(allActions);
+			setCurrentConnections(allConnections);
+		}
+	}, [filter, allDataObjects, allActions, allConnections]);
+
 
 	return (
 		<Sheet sx={{ display: 'flex', height: "100%", flexDirection: 'column' }}>
 			<PageHeader title={'Configuration'} noBack={true} />
 			<Sheet sx={{ display: 'flex', height: "calc(100% - 6.6rem)"}}>
-				<ElementList data={data} width={listWidth} mainRef={listRef} />
+				<ElementList dataObjects={currentDataObjects} actions={currentActions} connections={currentConnections} width={listWidth} mainRef={listRef} filter={filter} setFilter={setFilter} />
 				<DraggableDivider setWidth={setListWidth} cmpRef={listRef} isRightCmp={false} />
 				<Routes>
 					<Route path='search/:ownSearchString' element={<SearchResults data={data}/>} /> //the ownSearchString is our definition of a search because of problems with routing Search Parameters
