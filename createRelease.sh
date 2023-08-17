402ec79dfa70d226853ff4996a06bebdf2fe8436
zkull <zach.kull@gmail.com>
2023-08-17 09:50:08 +0200

    include images in release

--- createRelease.sh
+++ createRelease.sh
@@ -4,6 +4,6 @@ yarn build
 
 pushd build
 
-tar -czvf ../sdl-visualizer.tar.gz static/css/*.css static/js/*.js index.html manifest.json sdl_logo192.png lighttpd.conf
+tar -czvf ../sdl-visualizer.tar.gz images/* static/css/*.css static/js/*.js index.html manifest.json sdl_logo192.png lighttpd.conf README.md build_index.sh build_index.py requirements.txt
 
 popd
