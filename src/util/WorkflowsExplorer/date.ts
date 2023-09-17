f1fb83b048b5b95ec55cf9f9bafd1b0a5e0500fc
zkull <zach.kull@gmail.com>
2023-09-17 23:15:37 +0200

    use client timezone to format dates

--- src/util/WorkflowsExplorer/date.ts
+++ src/util/WorkflowsExplorer/date.ts
@@ -1,40 +1,27 @@
 import spacetime from 'spacetime';
 
+const clientTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
 export const formatTimestamp = (date: Date, timezone?: string): string => {
-  if (!date) return date; 
-  if (timezone) {
-    return spacetime(date, 'GMT+0').goto(`${timezone}`).unixFmt('dd.MM.yyyy HH:mm:ss');
-  }
-  return spacetime(date, 'GMT+0').unixFmt('dd.MM.yyyy HH:mm:ss');
+  if (!date) return '-'; 
+  return spacetime(date, 'GMT+0').goto(timezone || clientTimezone).unixFmt('dd.MM.yyyy HH:mm:ss');
 };
 
 export const getTimestampString = (date: Date, timezone?: string): string => {
-  if (timezone) {
-    return spacetime(date, 'GMT+0').goto(`${timezone}`).unixFmt('HH:mm:ss');
-  }
-  return spacetime(date, 'GMT+0').unixFmt('HH:mm:ss');
+  return spacetime(date, 'GMT+0').goto(timezone || clientTimezone).unixFmt('HH:mm:ss');
 };
 
 export const getTimeRangeString = (date: Date, timezone?: string): string => {
-  if (timezone) {
-    return spacetime(date, 'GMT+0').goto(`${timezone}`).unixFmt('yyyy-MM-dd HH:mm');
-  }
-  return spacetime(date, 'GMT+0').unixFmt('yyyy-MM-dd HH:mm');
+  return spacetime(date, 'GMT+0').goto(timezone || clientTimezone).unixFmt('yyyy-MM-dd HH:mm');
 };
 
 export const getDateTimeLocalString = (date: Date, timezone?: string): string => {
-  if (timezone) {
-    return spacetime(date, 'GMT+0').goto(`${timezone}`).unixFmt('yyyy-MM-dd HH:mm:ss');
-  }
-  return spacetime(date, 'GMT+0').unixFmt('yyyy-MM-dd HH:mm:ss');
+  return spacetime(date, 'GMT+0').goto(timezone || clientTimezone).unixFmt('yyyy-MM-dd HH:mm:ss');
 };
 
 // Return timepoint X days from now.
 export const getTimeFromPastByDays = (days: number, timezone?: string): number => {
-  if (timezone) {
-    return spacetime.now(`${timezone}`).subtract(days, 'day').startOf('day').epoch;
-  }
-  return spacetime(Date.now() - 1000 * 60 * 60 * 24 * days).startOf('day').epoch;
+  return spacetime.now(timezone || clientTimezone).subtract(days, 'day').startOf('day').epoch;
 };
 
 export const durationMillis = (duration: string) => {
