d6161b5861a0926037d3b736df939feea114a8ec
nguyenchithanh281195 <32167655+nguyenchithanh281195@users.noreply.github.com>
2023-12-01 01:03:26 +0700

    Adapt call to rest api to work similar as local file (#67)
    * Adapt call to rest api to work similar as local file

--- src/api/fetchAPI_rest.ts
+++ src/api/fetchAPI_rest.ts
@@ -2,26 +2,39 @@ import { fetchAPI } from "./fetchAPI";
 
 export class fetchAPI_rest implements fetchAPI {
     url: string;
-    
+
     constructor(url: string) {
         this.url = url;
     }
 
+    private fetch(url: string) {
+        return fetch(url, { mode: "cors" })
+            .then((res) => res.json())
+            .then((runs) =>
+            runs.map((run) => {
+                // convert date strings to date
+                run.runStartTime = new Date(run.runStartTime);
+                run.attemptStartTime = new Date(run.attemptStartTime);
+                run.attemptStartTimeMillis = new Date(run.attemptStartTime).getTime(); // needed for HistorBarChart
+                run.runEndTime = new Date(run.runEndTime);
+                run.duration =
+                run.runEndTime.getTime() - run.attemptStartTime.getTime();
+                return run;
+            })
+            );
+    }
+
     getWorkflows = () => {
-        return fetch(`${this.url}/workflows`, {mode:'cors'})
-        .then(res => res.json())
-        .catch(err => console.log("Unexpected Server error: ", err))
-    };
-    
-    
+        return this.fetch(`${this.url}/workflows`)
+            .catch((err) => console.log("Unexpected Server error: ", err));
+    };  
+
     getWorkflowRuns = (name: string) => {
-        return fetch(`${this.url}/workflow?name=${name}`, {mode:'cors'})
-        .then(res => res.json())
+        return this.fetch(`${this.url}/workflow?name=${name}`);
     };
-    
-    
-    getRun = (args: {name: string, runId: number, attemptId: number}) => {
-        return fetch(`${this.url}/run?name=${args.name}&runId=${args.runId}&attemptId=${args.attemptId}`, {mode:'cors'})
-        .then(res => res.json())
-    };   
+
+    getRun = (args: { name: string; runId: number; attemptId: number }) => {
+        return fetch(`${this.url}/run?name=${args.name}&runId=${args.runId}&attemptId=${args.attemptId}`, { mode: "cors" })
+        .then((res) => res.json());
+    };
 }
