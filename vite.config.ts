b1c6ac4ad68e6ca93c5f518a9dda63a6b3407ca4
zkull <zach.kull@gmail.com>
2023-11-30 08:07:03 +0100

    fix build / hocon-parser

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
       publicDir: './public',
       commonjsOptions: {
         dynamicRequireTargets: [
-          "node_modules/@pushcorn/hocon-parser"
+          "node_modules/@pushcorn/hocon-parser/lib/**/*.js"
         ],
       },
       rollupOptions: {
