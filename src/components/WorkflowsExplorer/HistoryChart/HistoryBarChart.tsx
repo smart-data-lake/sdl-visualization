fa6b1861b9efc3f06d718157461b1a44923bee3a
zku <zacharias.kull@elca.ch>
2025-01-13 21:54:11 +0100

    HistoryBarChart: assert xAxis is sorted

--- src/components/WorkflowsExplorer/HistoryChart/HistoryBarChart.tsx
+++ src/components/WorkflowsExplorer/HistoryChart/HistoryBarChart.tsx
@@ -4,6 +4,7 @@ import { useWorkspace } from "../../../hooks/useWorkspace";
 import { formatDuration } from "../../../util/WorkflowsExplorer/format";
 import { getStatusColor } from "../Timeline/TimelineRow/utils";
 import { CustomTooltip } from "./CustomTooltip";
+import { compareFunc } from "../../../util/helpers";
 
 const HistoryBarChart = (props: {runs: any[], selectRange: (range: [Date,Date]) => void}) => {
     const { runs } = props;
@@ -17,17 +18,21 @@ const HistoryBarChart = (props: {runs: any[], selectRange: (range: [Date,Date]) 
       setRefArea([]); // reset
     }
 
+    const xAxisAttr = "attemptStartTimeMillis"
+    const yAxisAttr = "duration"
+    const sortedRuns = runs.sort(compareFunc(xAxisAttr))
+
 
     //TODO: ticks on y axis are not yet optimal
     // logarithmic scale, or tune maxY?
     //const scale = scaleLog().base(Math.E);
-    const maxY = (runs.length > 0 ? runs.map(run => run.duration as number).reduce((max, cur) => Math.max(max,cur)) : 1);
+    const maxY = (runs.length > 0 ? runs.map(run => run[yAxisAttr] as number).reduce((max, cur) => Math.max(max,cur)) : 1);
     const maxYFloorMinute = Math.max(Math.floor(maxY/1000/60)*1000*60, 1000*60); // value for main tick on y axis, minimum is 1 minute
     const maxYCeilMinute = maxYFloorMinute + 1000*60; // add an additional minute for the domain max y value
 
     return (
       <ResponsiveContainer height={140} debounce={10}>
-        <BarChart data={runs}
+        <BarChart data={sortedRuns}
                   onMouseDown={(e: any) => setRefArea((e && e.activeLabel) ? [e.activeLabel as number] : [])}
                   onMouseMove={(e: any) => refArea.length > 0 && setRefArea((e && e.activeLabel) ? [refArea[0], e.activeLabel as number] : [])}
                   onMouseUp={(e: any) => (e && e.activeLabel) ? handleSelectRange(e.activeLabel as number) : setRefArea([])}
@@ -40,9 +45,9 @@ const HistoryBarChart = (props: {runs: any[], selectRange: (range: [Date,Date]) 
               />
             <CartesianGrid vertical={false} strokeDasharray="3" />
             <YAxis width={77} ticks={[0, maxYFloorMinute]} domain={[0, maxYCeilMinute]} tickFormatter={(value) => formatDuration(value)} scale="linear"/>
-            <XAxis dataKey="attemptStartTimeMillis" tickFormatter={(value) => new Date(value).toLocaleDateString(undefined, {year: 'numeric', month: 'short', day: 'numeric'})} padding={'gap'} minTickGap={30}/>
+            <XAxis dataKey={xAxisAttr} tickFormatter={(value) => new Date(value).toLocaleDateString(undefined, {year: 'numeric', month: 'short', day: 'numeric'})} padding={'gap'} minTickGap={30}/>
             <Bar 
-                dataKey="duration" 
+                dataKey={yAxisAttr}
                 stackId="a" 
                 fill="#20af2e"
                 onClick={data => navigateRel(`${data.runId}.${data.attemptId}/timeline`)}
@@ -51,7 +56,7 @@ const HistoryBarChart = (props: {runs: any[], selectRange: (range: [Date,Date]) 
                 minPointSize={5}
                 isAnimationActive={false}
                 >
-                  {runs.map((entry, index) => <Cell key={`cell-${index}`} fill={getStatusColor(entry.status)} style={{cursor: 'pointer'}} />)}    
+                  {sortedRuns.map((entry, index) => <Cell key={`cell-${index}`} fill={getStatusColor(entry.status)} style={{cursor: 'pointer'}} />)}    
             </Bar>
             {refArea && refArea.length == 2 && <ReferenceArea yAxisId="0" ifOverflow="hidden" x1={refArea[0]} x2={refArea[1]} strokeOpacity={0.3}/>}            
           </BarChart>
